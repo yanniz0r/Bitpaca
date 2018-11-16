e60ed601b6646220842abe5a12989f2dab01a71e
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-11-16 19:58:15 +0100

    Adds Sections to DesingerPage

--- src/pages/designer-page/index.tsx
+++ src/pages/designer-page/index.tsx
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 import Sidebar from '../../components/sidebar'
+import styled from 'styled-components';
 
+const PageWrapper = styled.div`
+  display: flex;
+`
+
+const SidebarSection = styled.div`
+  width: 25%;
+`
+
+const MainSection = styled.div`
+  width: 75%;
+`
 class DesignerPage extends Component {
 
   render = () => {
-    return <Sidebar/>
+    return <PageWrapper>
+      <SidebarSection>
+        <Sidebar/>
+      </SidebarSection>
+      <MainSection>
+        <h1>Hello!</h1>
+      </MainSection>
+    </PageWrapper>
   }
 
 }
