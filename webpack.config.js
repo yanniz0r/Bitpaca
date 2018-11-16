9c65538e8c897ea35c551b1601b6f479ca770c99
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-11-16 17:12:20 +0100

    Some setup

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: './src/app.ts',
+  output: {
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: 'ts-loader' }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'BitPaca'
+    })
+  ]
+};
