1afaa5f92e70a784511157005e9d7db7429445c7
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-11-16 17:52:39 +0100

    Adds css and some components

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import GlobalStyle from './components/global-style'
+
+
+const App = () => (
+  <>
+    <GlobalStyle/>
+    <h1>Hello World!</h1>
+  </>
+)
+
+export default App
