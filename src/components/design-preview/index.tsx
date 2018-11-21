dd9a100281dd7ada55fb7f150aadbe8a5a8f5af3
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-11-21 22:39:19 +0100

    Adds state based rendering

--- src/components/design-preview/index.tsx
+++ src/components/design-preview/index.tsx
@@ -3,7 +3,9 @@ import styled from 'styled-components'
 import { body1, blanket1 } from '../../library/asset-catalouge'
 import Dimension from '../../library/dimension'
 import background from '../../assets/backgrounds/mountains.png'
-import { observer } from 'mobx-react'
+import { inject, observer } from 'mobx-react'
+import Design from '../../models/design'
+import { autorun } from 'mobx';
 
 const DPWrapper = styled.div`
   flex-grow: 1;
@@ -20,38 +22,58 @@ const Canvas = styled.canvas`
   overflow: hidden;
 `
 
-class DesignPreview extends Component {
+export interface DesignPreviewProps {
+  design?: Design
+}
+
+@inject('design')
+@observer
+class DesignPreview extends Component<DesignPreviewProps> {
 
   private canvas: HTMLCanvasElement
   private canvasContext: CanvasRenderingContext2D
+  private disposer: () => any
 
-  public componentDidMount = () => {
+  public renderAssets() {
+    const { design } = this.props
+    design.assets.forEach((asset) => {
+      console.log(asset.loading)
+      this.canvasContext.drawImage(
+        asset.getImage(), 
+        asset.getLocation().x, 
+        asset.getLocation().y, 
+        asset.getDimension().x, 
+        asset.getDimension().y
+      )
+    })
+  }
+
+  private resetCanvas() {
+    this.canvasContext.clearRect(0, 0, this.canvas.width, this.canvas.height);
+  }
+
+  public componentDidMount() {
+    const { design } = this.props
+    if(design.loading) return
     this.canvasContext = this.canvas.getContext('2d')
     const canvasDimension: Dimension = { x: 2000, y: 2000 }
     this.canvas.width = canvasDimension.x
     this.canvas.height = canvasDimension.y
-
-    const assets = [
-      body1,
-      blanket1
-    ]
-
-    assets.forEach(asset => {
-      asset.getImage().onload = () => {
-        this.canvasContext.drawImage(
-          asset.getImage(), 
-          asset.getLocation().x, 
-          asset.getLocation().y, 
-          asset.getDimension(canvasDimension).x, 
-          asset.getDimension(canvasDimension).y
-        )
-      }
+    this.resetCanvas()
+    this.disposer = autorun(() => {
+      this.renderAssets()
     })
   }
 
+  public componentWillUnmount() {
+    if(this.disposer) this.disposer()
+  }
+
   public render = () => {
+    const { design } = this.props
     return <DPWrapper>
-        <Canvas ref={ref => this.canvas = ref}></Canvas>
+        { design.loading && 'Loading...'}
+        { !design.loading && <Canvas ref={ref => this.canvas = ref}/> }
       </DPWrapper>
   }
 
