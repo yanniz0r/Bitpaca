09e277fae57a4c69bb97bc0c1ef69e20e036b7eb
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-11-21 10:03:32 +0100

    Adds position to asset

--- src/library/asset-catalouge.ts
+++ src/library/asset-catalouge.ts
@@ -1,4 +1,5 @@
 import Asset from './asset'
+import * as DesignPositions from './design-positions'
 
-export const body1 = new Asset('body/body.png', { x: 1381, y: 1968 }, { x: 300, y: 20})
-export const blanket1 = new Asset('accessories/blanket.png', { x: 624, y: 384 }, { x: 800, y: 880})
+export const body1 = new Asset('body/body.png', { x: 1381, y: 1968 }, { x: 300, y: 20}, DesignPositions.body)
+export const blanket1 = new Asset('accessories/blanket.png', { x: 624, y: 384 }, { x: 800, y: 880}, DesignPositions.back)
--- src/library/asset.ts
+++ src/library/asset.ts
@@ -1,16 +1,19 @@
 import Dimension from './dimension'
+import DesignPosition from './design-position'
 
 class Asset {
 
   private imagePath: string
   private image: HTMLImageElement = new Image()
   private dimension: Dimension
-  private position: Dimension
+  private location: Dimension
+  private position: DesignPosition
 
-  constructor(path: string, dimension: Dimension, position: Dimension) {
+  constructor(path: string, dimension: Dimension, location: Dimension, position: DesignPosition) {
     this.imagePath = require(`../assets/alpaca/${path}`)
     this.image.src = this.imagePath
     this.dimension = dimension
+    this.location = location
     this.position = position
   }
 
@@ -21,6 +24,8 @@ class Asset {
   public getDimension = (canvasDimension: Dimension = null) => {
     return this.dimension
   }
+
+  public getLocation = () => this.location
 
   public getPosition = () => this.position
 
