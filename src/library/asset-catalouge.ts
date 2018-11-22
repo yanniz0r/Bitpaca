099aaf31bf3a56639ffe18c68eb4b561466261b1
Yannic Inselmann <yannic.inselmann@sinnerschrader.com>
2018-11-22 10:35:12 +0100

    Fixes garland

--- src/library/asset-catalouge.ts
+++ src/library/asset-catalouge.ts
@@ -4,4 +4,4 @@ import * as DesignPositions from './design-positions'
 export const body1 = new Asset('body/body1.png', { x: 1381, y: 1968 }, { x: 300, y: 20}, DesignPositions.body)
 export const body2 = new Asset('body/body2.png', { x: 1381, y: 1968 }, { x: 300, y: 20}, DesignPositions.body)
 export const blanket1 = new Asset('accessories/blanket.png', { x: 624, y: 384 }, { x: 800, y: 880}, DesignPositions.back)
-export const garland1 = new Asset('accessories/garland.png', { x: 624, y: 384 }, { x: 800, y: 880}, DesignPositions.back)
+export const garland1 = new Asset('accessories/garland.png', { x: 540, y: 484 }, { x: 320, y: 550}, DesignPositions.neck)
--- src/library/design-positions.ts
+++ src/library/design-positions.ts
@@ -2,3 +2,4 @@ import DesignPosition from './design-position'
 
 export const body = new DesignPosition('body', false)
 export const back = new DesignPosition('back')
+export const neck = new DesignPosition('neck')
